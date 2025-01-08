a39d16851e6266bf922eda3332b634653e0aabd9
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-01-09 01:14:47 +0400

    bir hata alıyorum

---
+++ ApiConsume/HotelProject.BusinessLayer/Concrete/TestimonialManager.cs
@@ -0,0 +1,41 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.DataAccessLayer.Abstarct;
+using HotelProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.BusinessLayer.Concrete
+{
+    public class TestimonialManager : ITestimonialService
+    {
+
+        private readonly ITestimonialDal _testimonialDal;
+        public void TDelete(Testimonial t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Testimonial TGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Testimonial> TGetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TInsert(Testimonial t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUptade(Testimonial t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
