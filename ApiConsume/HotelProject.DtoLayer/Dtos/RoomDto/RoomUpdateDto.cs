f53b32ff36c509e5b5b450d272ecdf05e282dee3
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-01-13 02:07:57 +0400

    Dto katmanında Room entitysiyle ilgili işlemler yaptım add ve updatelerini ekledim

--- ApiConsume/HotelProject.DtoLayer/Dtos/RoomDto/RoomUpdateDto.cs
+++ ApiConsume/HotelProject.DtoLayer/Dtos/RoomDto/RoomUpdateDto.cs
@@ -7,22 +7,28 @@ using System.Threading.Tasks;
 
 namespace HotelProject.DtoLayer.Dtos.RoomDto
 {
-    public class RoomAddDto
+    public class RoomUpdateDto
     {
+       
+        public int RoomID { get; set; }
+
         [Required(ErrorMessage = "Lütfen oda numarası giriniz")]
-        public string RoomNumber { get; set; }  
+        public string RoomNumber { get; set; }
+        [Required(ErrorMessage = "Lütfen oda görseli giriniz")]
         public string RoomCoverImage { get; set; }
 
-        [Required(ErrorMessage ="Lütfen Fiyat giriniz")]
-        public int Price { get; set; } 
+        [Required(ErrorMessage = "Lütfen Fiyat giriniz")]
+        public int Price { get; set; }
         [Required(ErrorMessage = "Lütfen Başlık giriniz")]
-        public string Title { get; set; } 
+        [StringLength(100,ErrorMessage ="Lütfen en fazla 100 karakter giriniz")]
+        public string Title { get; set; }
         [Required(ErrorMessage = "Lütfen Yatak sayısı giriniz")]
-        public string BedCount { get; set; } 
+        public string BedCount { get; set; }
         [Required(ErrorMessage = "Lütfen Banyo sayısı giriniz")]
         public string BathCount { get; set; }
         public string Wifi { get; set; }
-        public string Description { get; set; }
 
+        [Required(ErrorMessage = "Lütfen Açıklamayı giriniz")]
+        public string Description { get; set; }
     }
 }
