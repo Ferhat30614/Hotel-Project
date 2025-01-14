f19b2726b05a23858d6398426d1211f795873800
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-01-14 22:18:35 +0400

    frontendimde Service dtomu tanımladım ve apiden liste getirme işlemini şimdilik yaptım

---
+++ Frontend/HotelProject.WebUI/Mapping/AutoMapperConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using HotelProject.EntityLayer.Concrete;
+using HotelProject.WebUI.Dtos.ServiceDto;
+
+namespace HotelProject.WebUI.Mapping
+{
+    public class AutoMapperConfig:Profile
+    {
+
+        public AutoMapperConfig()
+        {
+            CreateMap<Service, CreateServiceDto>().ReverseMap();
+            CreateMap<Service, ResultServiceDto>().ReverseMap();
+            CreateMap<Service, UpdateServiceDto>().ReverseMap();
+        }
+
+    }
+}
