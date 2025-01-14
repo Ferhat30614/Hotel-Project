53d6e1775aa1fd7db3862737c71b3dcf3db2b522
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-01-14 23:40:41 +0400

    Servicede sorunsuz ekleme işlemi tmm

--- Frontend/HotelProject.WebUI/Controllers/ServiceController.cs
+++ Frontend/HotelProject.WebUI/Controllers/ServiceController.cs
@@ -1,7 +1,7 @@
 ﻿using HotelProject.WebUI.Dtos.ServiceDto;
-using HotelProject.WebUI.Models.Staff;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace HotelProject.WebUI.Controllers
 {
@@ -32,6 +32,46 @@ namespace HotelProject.WebUI.Controllers
 
             return View();
         }
+
+
+
+        [HttpGet]
+        public IActionResult AddService()
+        {
+
+            return View();
+
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> AddService(CreateServiceDto createServiceDto)
+        {
+            if (!ModelState.IsValid) 
+            {
+                return View();
+            
+            }
+            var client = _httpClientFactory.CreateClient();
+
+            var jsondata = JsonConvert.SerializeObject(createServiceDto);
+
+            StringContent stringContent = new StringContent(jsondata, Encoding.UTF8, "application/json");
+
+            var responseMessage = await client.PostAsync("http://localhost:5045/api/Service", stringContent);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+
+                return RedirectToAction("Index");
+
+            }
+
+            return View();
+
+
+        }
+
 
 
     }
