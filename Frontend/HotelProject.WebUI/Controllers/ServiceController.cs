83cd9316de3c23bf5ca1fa17a18a973283f36477
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-01-15 01:19:59 +0400

    Service dto için delete ve update işlemlerini ekledim ama updatede istediğim sonuçları tam olarak alamıyorum...

--- Frontend/HotelProject.WebUI/Controllers/ServiceController.cs
+++ Frontend/HotelProject.WebUI/Controllers/ServiceController.cs
@@ -72,8 +72,6 @@ namespace HotelProject.WebUI.Controllers
 
         }
 
-
-
         public async Task<IActionResult> DeleteService(int id)
         {
 
@@ -92,6 +90,62 @@ namespace HotelProject.WebUI.Controllers
             return View();
 
         }
+
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateService(int id)
+        {
+
+            var client = _httpClientFactory.CreateClient();
+
+            var responseMessage = await client.GetAsync($"http://localhost:5045/api/Service/{id}");
+
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var dataJson = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateServiceDto>(dataJson);
+
+                return View(values);
+
+            }
+            return View();
+
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateService(UpdateServiceDto updateServiceDto)
+        {
+            if (!ModelState.IsValid) 
+            {
+
+                return View();
+
+
+            }
+
+            var client = _httpClientFactory.CreateClient();
+            var dataJson = JsonConvert.SerializeObject(updateServiceDto);
+            StringContent stringContent = new StringContent(dataJson, Encoding.UTF8, "application/json");
+
+            var responseMessage = await client.PutAsync($"http://localhost:5045/api/Service/", stringContent);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+
+                return RedirectToAction("Index");
+
+
+            }
+            return View();
+
+
+
+        }
+
+
+
 
 
 
