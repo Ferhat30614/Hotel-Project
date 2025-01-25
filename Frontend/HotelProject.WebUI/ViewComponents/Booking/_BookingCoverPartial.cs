4b342b3aa6d51bfd311e6a1b9ed45c2fa9f5940d
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-01-26 01:33:03 +0400

    düznelemeler

---
+++ Frontend/HotelProject.WebUI/ViewComponents/Booking/_BookingCoverPartial.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebUI.ViewComponents.Booking
+{
+    public class _BookingCoverPartial:ViewComponent
+    {
+
+
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+
+    }
+}
