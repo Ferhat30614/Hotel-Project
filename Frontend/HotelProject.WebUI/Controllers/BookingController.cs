76963279ccf0fdaceddea4be16333d38f29ef97b
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-01-27 19:04:33 +0400

    booking için iligili eklemeleri yaptım  post işlemine devam

--- Frontend/HotelProject.WebUI/Controllers/BookingController.cs
+++ Frontend/HotelProject.WebUI/Controllers/BookingController.cs
@@ -1,9 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Dtos.BookingDto;
+using HotelProject.WebUI.Dtos.ServiceDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace HotelProject.WebUI.Controllers
 {
     public class BookingController : Controller
     {
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BookingController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
         public IActionResult Index()
         {
             return View();
@@ -16,8 +27,25 @@ namespace HotelProject.WebUI.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> AddBooking(string a)
+        public async Task<IActionResult> AddBooking(CreateBookingDto createBookingDto)
         {
+            createBookingDto.Status = "Onay Bekliyor.";
+           
+            var client = _httpClientFactory.CreateClient();
+
+            var jsondata = JsonConvert.SerializeObject(createBookingDto);
+
+            StringContent stringContent = new StringContent(jsondata, Encoding.UTF8, "application/json");
+
+            var responseMessage = await client.PostAsync("http://localhost:5045/api/Booking", stringContent);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+
+                return RedirectToAction("Index");
+
+            }
+
             return View();
         }
 
