a64c22dc7e65a03d9b6199f1d13e3ac9fb549371
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-01-29 01:23:26 +0400

    Exchange rates apisinden ilgili verileri çekme tamam

---
+++ RapidApi/RapidApiConsume/Controllers/ExchangeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApiConsume.Models;
+
+namespace RapidApiConsume.Controllers
+{
+    public class ExchangeController : Controller
+    {
+        
+        public async Task<IActionResult> Index()
+        {
+           
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://booking-com15.p.rapidapi.com/api/v1/meta/getExchangeRates?base_currency=TRY"),
+                Headers =
+    {
+        { "x-rapidapi-key", "6b99735555msh46fe40a0ee44ff2p1db52bjsnc05306989038" },
+        { "x-rapidapi-host", "booking-com15.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(body);
+
+                var values = JsonConvert.DeserializeObject<ExchangeViewModel>(body);
+                
+                
+
+                return View(values.data.exchange_rates.ToList());
+            }
+        }
+    }
+}
---
+++ RapidApi/RapidApiConsume/Models/ExchangeViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace RapidApiConsume.Models
+{
+    public class ExchangeViewModel
+    {
+        public Data data { get; set; } // API'den dönen "data" objesini modelle
+
+        public class Data
+        {
+            public List<Exchange_Rates> exchange_rates { get; set; }
+            public string base_currency { get; set; }
+            public string base_currency_date { get; set; }
+        }
+
+        public class Exchange_Rates
+        {
+            public string exchange_rate_buy { get; set; }
+            public string currency { get; set; }
+        }
+
+
+    }
+}
