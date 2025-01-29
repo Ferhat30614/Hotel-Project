cab864687efff2cf807881a96ff2933715dd6ee2
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-01-29 18:13:30 +0400

    Booking searc işlemlerine başladık

---
+++ RapidApi/RapidApiConsume/Controllers/SearchBookingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RapidApiConsume.Controllers
+{
+    public class SearchBookingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
