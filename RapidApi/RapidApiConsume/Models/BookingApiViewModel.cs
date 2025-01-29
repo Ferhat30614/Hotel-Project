dda2d5093c7de665164a1f993ec0dd1500f903b1
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-01-29 17:02:47 +0400

    Model sınıfımı epey sadeleştirdim

--- RapidApi/RapidApiConsume/Models/BookingApiViewModel.cs
+++ RapidApi/RapidApiConsume/Models/BookingApiViewModel.cs
@@ -11,8 +11,7 @@
         public class Data
         {
             public Hotel[] hotels { get; set; }
-            public Meta[] meta { get; set; }
-            public Appear[] appear { get; set; }
+           
         }
 
         public class Hotel
@@ -62,132 +61,15 @@
 
         public class Pricebreakdown
         {
-            public Excludedprice excludedPrice { get; set; }
-            public Benefitbadge[] benefitBadges { get; set; }
-            public Grossprice grossPrice { get; set; }
-            public Strikethroughprice strikethroughPrice { get; set; }
+     
             public object[] taxExceptions { get; set; }
         }
 
-        public class Excludedprice
-        {
-            public string currency { get; set; }
-            public float value { get; set; }
-        }
-
-        public class Grossprice
-        {
-            public float value { get; set; }
-            public string currency { get; set; }
-        }
-
-        public class Strikethroughprice
-        {
-            public float value { get; set; }
-            public string currency { get; set; }
-        }
-
-        public class Benefitbadge
-        {
-            public string identifier { get; set; }
-            public string text { get; set; }
-            public string variant { get; set; }
-            public string explanation { get; set; }
-        }
 
         public class Checkout
         {
             public string untilTime { get; set; }
             public string fromTime { get; set; }
-        }
-
-        public class Meta
-        {
-            public string title { get; set; }
-        }
-
-        public class Appear
-        {
-            public Component component { get; set; }
-            public string id { get; set; }
-            public string contentUrl { get; set; }
-        }
-
-        public class Component
-        {
-            public Props props { get; set; }
-        }
-
-        public class Props
-        {
-            public bool fill { get; set; }
-            public Content content { get; set; }
-            public string title { get; set; }
-            public string text { get; set; }
-        }
-
-        public class Content
-        {
-            public Props1 props { get; set; }
-        }
-
-        public class Props1
-        {
-            public bool fitContentWidth { get; set; }
-            public Item[] items { get; set; }
-        }
-
-        public class Item
-        {
-            public Props2 props { get; set; }
-        }
-
-        public class Props2
-        {
-            public Component1 component { get; set; }
-        }
-
-        public class Component1
-        {
-            public Props3 props { get; set; }
-        }
-
-        public class Props3
-        {
-            public string spacing { get; set; }
-            public Item1[] items { get; set; }
-            public string variant { get; set; }
-            public string icon { get; set; }
-            public string accessibilityLabel { get; set; }
-            public string tertiaryTintedColor { get; set; }
-        }
-
-        public class Item1
-        {
-            public Props4 props { get; set; }
-        }
-
-        public class Props4
-        {
-            public Text[] text { get; set; }
-        }
-
-        public class Text
-        {
-            public string font { get; set; }
-            public string text { get; set; }
-            public string color { get; set; }
-            public Linkaction[] linkActions { get; set; }
-        }
-
-        public class Linkaction
-        {
-            public Props5 props { get; set; }
-        }
-
-        public class Props5
-        {
-            public string url { get; set; }
         }
 
 
