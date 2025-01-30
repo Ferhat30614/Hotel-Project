9e3566817f433f7a30ae9b143bd1df346f5ab938
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-01-30 23:48:36 +0400

    isme göre id de tamam rapid api işlemleri bukadar olsun  şimdilik

---
+++ RapidApi/RapidApiConsume/Controllers/BookingByCityController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApiConsume.Models;
+
+namespace RapidApiConsume.Controllers
+{
+    public class BookingByCityController : Controller
+    {
+        public async Task<IActionResult> Index(string cityId)
+        {
+
+            if (!string.IsNullOrEmpty(cityId))
+            {
+
+                var client = new HttpClient();
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri($"https://booking-com15.p.rapidapi.com/api/v1/hotels/searchHotels?dest_id={cityId}&search_type=CITY&arrival_date=2025-01-29&departure_date=2025-01-31&adults=1&children_age=0%2C17&room_qty=1&page_number=1&units=metric&temperature_unit=c&languagecode=en-us&currency_code=AED"),
+                    Headers =
+    {
+        { "x-rapidapi-key", "6b99735555msh46fe40a0ee44ff2p1db52bjsnc05306989038" },
+        { "x-rapidapi-host", "booking-com15.p.rapidapi.com" },
+    },
+                };
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var body = await response.Content.ReadAsStringAsync();
+                    var values = JsonConvert.DeserializeObject<BookingApiViewModel>(body);
+                    return View(values.data.hotels.ToList());
+                }
+
+            }
+            else {
+
+
+                var client = new HttpClient();
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri("https://booking-com15.p.rapidapi.com/api/v1/hotels/searchHotels?dest_id=-2092174&search_type=CITY&arrival_date=2025-01-29&departure_date=2025-01-31&adults=1&children_age=0%2C17&room_qty=1&page_number=1&units=metric&temperature_unit=c&languagecode=en-us&currency_code=AED"),
+                    Headers =
+    {
+        { "x-rapidapi-key", "6b99735555msh46fe40a0ee44ff2p1db52bjsnc05306989038" },
+        { "x-rapidapi-host", "booking-com15.p.rapidapi.com" },
+    },
+                };
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var body = await response.Content.ReadAsStringAsync();
+                    var values = JsonConvert.DeserializeObject<BookingApiViewModel>(body);
+                    return View(values.data.hotels.ToList());
+                }
+
+            }
+         
+        }
+    }
+}
