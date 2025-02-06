667246bea7f6b8980ef9b3baba4650dbe042a31a
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-02-07 01:16:49 +0400

    Guest ile ilgili işlemler api tarafında tamam gibi

---
+++ ApiConsume/HotelProject.WebApi/Controllers/GuestController.cs
@@ -0,0 +1,62 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GuestController : ControllerBase
+    {
+
+
+        private readonly IGuestService _guestService;
+
+        public GuestController(IGuestService GuestService)
+        {
+            _guestService = GuestService;
+        }
+
+        [HttpGet]
+        public IActionResult GuestList()
+        {
+            var values = _guestService.TGetList();
+
+            return Ok(values);
+
+        }
+        [HttpPost]
+        public IActionResult AddGuest(Guest staf)
+        {
+            _guestService.TInsert(staf);
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteGuest(int id)
+        {
+            var values = _guestService.TGetById(id);
+            _guestService.TDelete(values);
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UptadeGuest(Guest Guest)
+        {
+            _guestService.TUptade(Guest);
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetGuest(int id)
+        {
+            var values = _guestService.TGetById(id);
+            return Ok(values);
+        }
+
+
+
+    }
+}
