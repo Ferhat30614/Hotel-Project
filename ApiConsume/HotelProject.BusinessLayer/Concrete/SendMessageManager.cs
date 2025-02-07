922a3bb643d1fe17e24e8ba873b21d8b45340e8b
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-02-07 21:12:43 +0400

    Sendmessage  tablosunu ekledim ve ilgili interface ve sınıflarını oluşturdum ilfiki jatmanlarda

---
+++ ApiConsume/HotelProject.BusinessLayer/Concrete/SendMessageManager.cs
@@ -0,0 +1,46 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.DataAccessLayer.Abstarct;
+using HotelProject.DataAccessLayer.EntitiyFramework;
+using HotelProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.BusinessLayer.Concrete
+{
+    public class SendMessageManager : ISendMessageService
+    {
+        private readonly ISendMessageDal _sendMessage;
+
+        public SendMessageManager(ISendMessageDal sendMessage)
+        {
+            _sendMessage = sendMessage;
+        }
+        public void TDelete(SendMessage t)
+        {
+            _sendMessage.Delete(t);
+        }
+
+        public SendMessage TGetById(int id)
+        {
+            return _sendMessage.GetById(id);
+        }
+
+        public List<SendMessage> TGetList()
+        {
+            return _sendMessage.GetList();
+        }
+
+        public void TInsert(SendMessage t)
+        {
+            _sendMessage.Insert(t);
+        }
+
+        public void TUptade(SendMessage t)
+        {
+            _sendMessage.Uptade(t);
+        }
+    }
+}
