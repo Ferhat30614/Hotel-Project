c55bdf6777da05274d432b1096d2d0a409ef9d06
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-02-09 00:35:39 +0400

    Mail göndermek için işlemlere koyuldum   controller tarfaı şimdilik boyle olcak.

---
+++ Frontend/HotelProject.WebUI/Controllers/AdminMailController.cs
@@ -0,0 +1,52 @@
+﻿using HotelProject.WebUI.Models.Mail;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using MimeKit;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class AdminMailController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Index(AdminMailViewModel model)
+        {
+            MimeMessage mimeMessage = new MimeMessage();
+
+
+            MailboxAddress mailboxAddressFrom = new MailboxAddress("HotelierAdmin","Bura bizim mail ");
+            mimeMessage.From.Add(mailboxAddressFrom);
+
+            MailboxAddress mailboxAddressTo = new MailboxAddress("User", model.ReceiverMail);
+            mimeMessage.To.Add(mailboxAddressFrom);
+
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody = model.Body;
+            mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+
+            mimeMessage.Subject = model.Subject;
+
+            SmtpClient client = new SmtpClient();
+            client.Connect("smtp.gmail.com",587,false);
+            client.Authenticate("mail","password key");
+            client.Send(mimeMessage);
+            client.Disconnect(true);
+
+            return View();  
+
+
+
+
+
+        }
+    }
+}
