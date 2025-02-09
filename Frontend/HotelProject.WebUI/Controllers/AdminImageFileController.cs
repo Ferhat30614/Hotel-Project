5f8b54660e561f0ef690b6f5b402a796338c8084
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-02-10 02:19:52 +0400

    Frontend tarafıda tamam ve sorunsuz bir şekilde çalısıyor fotograf vs ekliyor

---
+++ Frontend/HotelProject.WebUI/Controllers/AdminImageFileController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class AdminImageFileController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(IFormFile file)
+        {
+
+            var stream=new MemoryStream();
+            await file.CopyToAsync(stream);
+
+            var bytes=stream.ToArray(); 
+
+            ByteArrayContent byteArrayContent = new ByteArrayContent(bytes);
+            byteArrayContent.Headers.ContentType = new MediaTypeHeaderValue(file.ContentType);
+
+            MultipartFormDataContent multipartFormDataContent=new MultipartFormDataContent();
+            multipartFormDataContent.Add(byteArrayContent,"file",file.FileName);
+
+            var httpclient = new HttpClient();
+            var response=await httpclient.PostAsync("http://localhost:5045/api/FileImage", multipartFormDataContent);
+
+            if (response.IsSuccessStatusCode)
+            {
+
+
+                return View();  
+            }
+            return View();  
+
+
+
+        }
+    }
+}
