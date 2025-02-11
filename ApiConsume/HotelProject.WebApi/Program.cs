e44d1e57c8f2c92e8b4e538305c4a6597f0969b8
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-02-12 03:49:42 +0400

    Work location tablom  için bachand tarafı tamam onun için api controlleride olusturdum ve sorunsuz çalışıyor...

--- ApiConsume/HotelProject.WebApi/Program.cs
+++ ApiConsume/HotelProject.WebApi/Program.cs
@@ -55,6 +55,10 @@ builder.Services.AddScoped<ISendMessageService, SendMessageManager>();
 builder.Services.AddScoped<IMessageCategoryDal, EfMessageCategoryDal>();
 builder.Services.AddScoped<IMessageCategoryService, MessageCategoryManager>();
 
+builder.Services.AddScoped<IWorkLocationDal, EfWorkLocationDal>();
+builder.Services.AddScoped<IWorkLocationService, WorkLocationManager>();
+
+
 builder.Services.AddAutoMapper(typeof(AutoMapperConfig));
 
 builder.Services.AddCors(opt=>
