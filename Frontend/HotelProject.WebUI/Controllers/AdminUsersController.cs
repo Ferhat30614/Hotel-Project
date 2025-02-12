4b3a0d7ba51d2a2470c8fc7338af6d16d020b336
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-02-12 23:21:46 +0400

    appuseri worklocation ile birlikte Listeleme yaparken hata alıyordum ama sorunu çözdüm

--- Frontend/HotelProject.WebUI/Controllers/AdminUsersController.cs
+++ Frontend/HotelProject.WebUI/Controllers/AdminUsersController.cs
@@ -1,26 +1,39 @@
 ﻿using HotelProject.EntityLayer.Concrete;
+using HotelProject.WebUI.Dtos.AppUserDto;
+using HotelProject.WebUI.Dtos.StaffDto;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace HotelProject.WebUI.Controllers
 {
     public class AdminUsersController : Controller
     {
-        private readonly UserManager<AppUser> _userManager;
-        public AdminUsersController(UserManager<AppUser> userManager)
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminUsersController(IHttpClientFactory httpClientFactory)
         {
-            _userManager = userManager;
+            _httpClientFactory = httpClientFactory;
         }
 
 
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            var values= _userManager.Users.ToList();    
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:5045/api/AppUser");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAppUserDto>>(jsonData);
+                return View(values);
+
+            }
 
 
-
-            return View(values);
+            return View();
         }
     }
 }
---
+++ Frontend/HotelProject.WebUI/Dtos/AppUserDto/ResultAppUserDto.cs
@@ -0,0 +1,23 @@
+﻿namespace HotelProject.WebUI.Dtos.AppUserDto
+{
+    public class ResultAppUserDto
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string City { get; set; }
+        public string ImageUrl { get; set; }
+
+        public string WorkDepartment { get; set; }
+        public int WorkLocationID { get; set; }
+        public WorkLocation WorkLocation { get; set; }
+    }
+
+
+    public class WorkLocation
+    {
+        public int WorkLocationID { get; set; }
+        public string WorkLocationName { get; set; }
+        public string WorkLocationCity { get; set; }
+    }
+
+}
