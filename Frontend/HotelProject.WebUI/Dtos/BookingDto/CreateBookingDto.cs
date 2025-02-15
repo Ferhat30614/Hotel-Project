421cc4d0f9c8918170d2960ffba5840d500eacae
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-02-15 14:15:38 +0400

    dtoya ekledin

--- Frontend/HotelProject.WebUI/Dtos/BookingDto/CreateBookingDto.cs
+++ Frontend/HotelProject.WebUI/Dtos/BookingDto/CreateBookingDto.cs
@@ -11,6 +11,8 @@
         public string RoomCount { get; set; }
         public string SpecialRequest { get; set; }
         public string Description { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
         public string Status { get; set; }
 
 
